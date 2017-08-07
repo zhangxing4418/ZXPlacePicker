9eb02469bc9967ceb4272eb831c40a665345c38c
ziv <ziv@zivdemac-mini.local>
2017-08-07 10:47:50 +0800

    Initial Commit

---
+++ ZXPlacePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXPlacePicker
+//
+//  Created by ziv on 2017/8/7.
+//  Copyright © 2017年 ziv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
